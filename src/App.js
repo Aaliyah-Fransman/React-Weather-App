0d2380af3ab93940644bbb859c7a92076713bef5
Aaliyah Fransman <Aaliyah.Fransman@gmail.com>
2025-11-21 14:02:12 +0000

    Added bootsrap to my project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by
+            <a href="https://github.com/Aaliyah-Fransman" target="_blank">
+              Aaliyah Fransman
+            </a>
+            and is
+            <a
+              href="https://github.com/Aaliyah-Fransman/My-Weather-App-Project"
+              target="_blank"
+            >
+              on GitHub
+            </a>
+            and
+            <a href="https://theskycast.netlify.app/?#" target="_blank">
+              hosted on Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
 }
