2842c13762a7d8fe7b1f2f1fe28f68f7d3902d6c
Aaliyah Fransman <Aaliyah.Fransman@gmail.com>
2025-12-02 18:35:24 +0000

    Refactored code

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           <p>
-            This project was coded by
+            This project was coded by{" "}
             <a href="https://github.com/Aaliyah-Fransman" target="_blank">
               Aaliyah Fransman
-            </a>
-            and is
+            </a>{" "}
+            and is{" "}
             <a
               href="https://github.com/Aaliyah-Fransman/My-Weather-App-Project"
               target="_blank"
             >
               on GitHub
-            </a>
-            and
+            </a>{" "}
+            and{" "}
             <a href="https://theskycast.netlify.app/?#" target="_blank">
               hosted on Netlify
             </a>
